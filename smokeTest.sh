9b553eedb973081b47f312d36bdad833146aafaa
anahtreek <keerthana.n_23@yahoo.com>
2018-11-28 16:25:23 +0530

    Create smokeTest.sh

---
+++ smokeTest.sh
@@ -0,0 +1,7 @@
+curl -X POST -H 'content-type: application/json;charset=UTF-8' -d '{"productName":"HD SetupBox", "serviceId":"100"}' 'https://productmicroservice.apps.dev.pcf-aws.com/product' > response
+grep '\"serviceId\":\"100\" 'response'
+if [ "$?" -eq "0" ] ; then
+echo "Success"
+else
+echo "Failed"
+fi
