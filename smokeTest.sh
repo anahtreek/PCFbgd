7faec055668532200787c5a85579710387d8e712
anahtreek <keerthana.n_23@yahoo.com>
2018-11-29 13:12:22 +0530

    Update smokeTest.sh

--- smokeTest.sh
+++ smokeTest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -X POST -H 'content-type: application/json;charset=UTF-8' -d '{"productName":"HD SetupBox", "serviceId":"100"}' 'https://product_wipro-new.apps.dev.pcf-aws.com/product' > response
+curl -X POST -H 'content-type: application/json;charset=UTF-8' -d '{"productName":"HD SetupBox", "serviceId":"100"}' 'https://product-new.apps.dev.pcf-aws.com/product' > response
 grep '\"serviceId\":\"100\" 'response'
 if [ "$?" -eq "0" ] ; then
 echo "Success"
